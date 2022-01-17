686749d5667989c3be8e9682e6fd5d389ae4b6cd
ericknathan <erick.capito@hotmail.com>
2022-01-17 13:11:13 -0300

    Debug de console desnecessário removido

--- src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -7,10 +7,6 @@ class SESMailProvider implements IMailProvider {
   private client: Transporter;
 
   constructor() {
-    console.log({
-      apiVersion: '2010-12-01',
-      region: process.env.AWS_REGION,
-    });
     this.client = nodemailer.createTransport({
       SES: new SES({
         apiVersion: '2010-12-01',
